b4c92e1b9e0bcaf4b9e876a8402bc0ad6491da21
Fabian Campoverde <fabi22isra@yahoo.com>
2024-12-22 12:19:03 -0500

    Update

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../App'; 
-import PaginaInicio from '../Content/pages/Pagina_inicio';
+import PaginaInicio from '../Content/pages/paginainicio';
 
 function AppRoutes() {
   return (
